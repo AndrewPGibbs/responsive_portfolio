814ce91b1aea50cf7bcaec4960759b92e0d4d84b
Andrew PG <andrewpgibbs71@gmail.com>
2024-06-11 14:03:18 -0400

    Banner update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,7 @@
+import { Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import HeaderImg from "../assets/header-img.svg";
+
 const Banner = () => {
     return ( 
         <section className="banner" id="home">
@@ -7,10 +11,14 @@ const Banner = () => {
                     <span className="tagline">Welcome to my Portfolio</span>
                     <h1>{`Hi I'm Andrew`}<span className="wrap">web developer</span></h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <button onClick={() => console.log('button pressed')}>Lets Connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+
+                    <Col xs={12} md={6} xl={15}>
+                    <img src={""} alt="Header image" />
                     </Col>
                 </Row>
             </Container>
-
         </section>
      );
 }
