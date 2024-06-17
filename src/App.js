160cec208d97c07a3f9bdd80afae4fdb824dfc2d
Andrew PG <andrewpgibbs71@gmail.com>
2024-06-17 02:54:05 -0400

    skills component and associated stylings

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header.js';
 import Banner from './components/Banner.js';
+import Skills from './components/Skills.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Skills />
     </div>
   );
 }
